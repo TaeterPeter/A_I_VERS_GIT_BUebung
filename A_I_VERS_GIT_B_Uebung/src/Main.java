32443264e1882f00c500062f2e4b4124df22e7cf
benni <benni@DESKTOP-C4E02LA.htl.grieskirchen.local>
2020-09-22 15:31:11 +0200

    added code in main

--- A_I_VERS_GIT_B_Uebung/src/Main.java
+++ A_I_VERS_GIT_B_Uebung/src/Main.java
@@ -1,3 +1,16 @@
+
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,8 +26,20 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        HttpServer server = HttpServerFactory.create("http://localhost:8080/rest");
+        server.start();
+    }
+    
+     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("nummer")
+    
+    public Integer patientenIDs() throws JSONException{
+       
+     
+        return 3;
     }
     
 }
